71a3af28ed1336f8bb8eccebc34aa9f58f1cfc90
Uel-5 <kariukiwaweru8156@gmail.com>
2024-05-28 13:08:14 +0300

    added configurations to config.py file

---
+++ server/config.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_sqlalchemy import flask_sqlalchemy # used to store data in the db.
+from flask_cors import CORS # used to send request to the backend from a different URL(frontend) for security.
+
+app = Flask(__name__) # initializes flask application.
+CORS(app) # blocks CORS errors that occur when sending requests from backend to frontend.
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///profile.db" # initialize database connection and name of db.
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False # Disable changes to database from getting tracked.
+
+
+db = SQLAlchemy(app) # Database instance which gives me access to the database connection from ("sqlite:///profile.db")
