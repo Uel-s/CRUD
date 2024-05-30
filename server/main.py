a95a6af0b07a1c5144401c7698c3d78da77ba680
Uel-5 <kariukiwaweru8156@gmail.com>
2024-05-30 11:56:01 +0300

    creating user done );

--- server/main.py
+++ server/main.py
@@ -13,7 +13,7 @@ def get_contacts():
 # Convert each contact object in the 'contacts' list to a JSON-serializable dictionary
 # using the 'to_json' method, and store the resulting list of dictionaries in 'json_contacts'.
     json_contacts = list(map(lambda x: x.to_json(), contacts))
-    return (jsonify({"contacts": json_contacts}) ,200,)# data is returned in json format
+    return (jsonify({"contacts": json_contacts})), 200 # data is returned in json format
 
 # Decorator for contact using the post method.
 
@@ -27,19 +27,22 @@ def create_contact():
         return(
             jsonify({"message": "Must include first name, last name and email"}), 400, # bad request
         )
-
+ # add to db.
     new_contact = Contact(first_name=first_name, last_name=last_name, email=email)
     try:
         db.session.add(new_contact)
         db.session.commit()
 
+# used to show errors when wrong data is entered in the db.
     except Exception as e:
-        return jsonify({"message":str(e)}), 400        
+        return jsonify({"message":str(e)}), 400  
+
+    # When user is created.
+
+    return jsonify({"message": "User Created!"}), 201
+              
 
     
-
-
-
 # Run the application.
 if __name__ == "__main__":
     # Check if the script is being run directly (not imported as a module).
